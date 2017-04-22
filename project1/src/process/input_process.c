f6e31366caeb8bd22b49f68a758c722e78a12e6e
taeseunglee <taeseunglee159@gmail.com>
2017-04-22 19:17:02 +0900

    Fix some bug

--- project1/src/process/input_process.c
+++ project1/src/process/input_process.c
@@ -4,22 +4,27 @@ void
 input_process(struct environment *env)
 {
   struct input_event ev[BUFF_SIZE];
-  size_t ret_bytes;
-  unsigned press_find = FALSE, i;
-  size_t sw_buff_size = sizeof(char) * MAX_BUTTON,
-         size_event = sizeof(struct input_event);
-
+  unsigned int press_find, i;
 
   /* get file descriptor from env */
-  int ev_fd = env->ev_fd,
-      push_switch_fd = env->push_switch_fd;
+  int ev_fd = env->ev_fd;
+  int push_switch_fd = env->push_switch_fd;
 
   /* setting message queue */
   int msqid, msgflg = IPC_CREAT | 0666;
-  message_buf mbuf;
-  unsigned char before_switch[MAX_BUTTON] = {0,};
-  memset(mbuf.mtext, 0, sizeof(mbuf.mtext));
-  size_t buf_length = sizeof(message_buf); 
+  message_buf switch_mbuf, key_mbuf;
+
+  size_t size_switch = sizeof(char) * MAX_BUTTON;
+  size_t size_event = sizeof(struct input_event);
+  size_t buf_length = sizeof(message_buf);
+  size_t size_mtext = sizeof(switch_mbuf.mtext);
+
+  /* Initialize variable */
+  switch_mbuf.mtype = MTYPE_SWITCH;
+  key_mbuf.mtype = MTYPE_READKEY;
+
+  memset(switch_mbuf.mtext, 0, size_mtext);
+  memset(key_mbuf.mtext, 0, size_mtext);
 
   if ((msqid = msgget(env->msg_key, msgflg)) < 0)
     {
@@ -31,34 +36,33 @@ input_process(struct environment *env)
   while (!quit)
     {
       // read an event
-      if ((ret_bytes = read(ev_fd, ev, size_event * BUFF_SIZE)) >= size_event) 
+      if (read(ev_fd, ev, size_event * BUFF_SIZE) >= size_event) 
         {
           if (ev[0].value == KEY_PRESS)
             {
-              ev[0].value = KEY_RELEASE; // prevent multiple 
-              mbuf.mtext[0] = ev[0].code; mbuf.mtype = MTYPE_READKEY;
-              MSGSND_OR_DIE(msqid, &mbuf, buf_length, IPC_NOWAIT);
+              ev[0].value = KEY_RELEASE; // prevent to read duplicate data
+              key_mbuf.mtext[0] = ev[0].code;
+              MSGSND_OR_DIE(msqid, &key_mbuf, buf_length, IPC_NOWAIT);
             }
         }
       usleep(100000);
 
-      read(push_switch_fd, &(mbuf.mtext), sw_buff_size);
-
+      /* read push-switch */
+      read(push_switch_fd, &(switch_mbuf.mtext), size_switch);
       press_find = FALSE, i = 8;
       do
         {
-          if (mbuf.mtext[i] && before_switch[i]) 
+          if (switch_mbuf.mtext[i])
             {
               press_find = TRUE;
               break;
             }
         } while(i--);
-      memcpy(before_switch, mbuf.mtext, sw_buff_size);
 
-      if (press_find) 
+      if (press_find)
         {
-          mbuf.mtype = MTYPE_SWITCH;
-          MSGSND_OR_DIE(msqid, &mbuf, buf_length, IPC_NOWAIT);
+          MSGSND_OR_DIE(msqid, &switch_mbuf, buf_length, IPC_NOWAIT);
+          memset(switch_mbuf.mtext, 0x00, size_mtext);
         }
     }
 
