088bc4e312b22138e36ffea1db1168d84a201f0a
taeseunglee <taeseunglee159@gmail.com>
2017-05-02 00:19:29 +0900

    Modify app.c and fix the bug of app.c, no initialization

--- project2/app/app.c
+++ project2/app/app.c
@@ -5,10 +5,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#ifdef linux
-#include <linux/compiler.h>
-#endif
-
 
 #ifndef likely
 # define likely(x) __builtin_expect(!!(x), 1)
@@ -24,16 +20,16 @@
 #define TRUE  1
 #define FALSE 0
 
-
+/*
 struct converted_argv
 {
   int time_interval;
   int number;
   int startup_option;
 };
-
+*/
 static inline int
-check_and_atoi (const char *str IN);
+atoi_and_check (const char *str IN);
 
 static inline struct converted_argv
 check_argv(char **argv IN);
@@ -48,7 +44,10 @@ main(int argc, char **argv)
   /* Arguments checking */
   if(argc != 4)
     {
-      printf("Usage : integer[1-100] integer[1-100] integer[0001-8000]\n");
+      printf("Usage : [1-100] [1-100] [0001-8000]\n"
+             "Note : time_interval(integer)[1-100]\n"
+             "\tnumber_of_times(integer)[1-100]\n"
+             "\tstartup_option(integer)[0001-8000]\n");
       printf("Note : time_interval number_of_times startup_option\n");
       return -1;
     }
@@ -60,27 +59,41 @@ main(int argc, char **argv)
       return -1;
     }
 
+  long time_interval, number, startup_option;
+  long packed_data;
+  
+  time_interval  = atoi_and_check(argv[1]);
+  number         = atoi_and_check(argv[2]);
+  startup_option = atoi_and_check(argv[3]);
 
-  write(timer_fd, &get_number, sizeof(get_number) );
+  printf("param : %ld %ld %ld\n", time_interval, number, startup_option);
+
+  packed_data = syscall(376, time_interval, number, startup_option);
+
+  printf("%ld ", (packed_data & 0xFF000000) >> 24);
+  printf("%ld ", (packed_data & 0x00FF0000) >> 16);
+  printf("%ld ", (packed_data & 0x0000FF00) >> 8);
+  printf("%ld\n", packed_data & 0x000000FF);
+
   close(timer_fd);
 
   return 0;
 }
 
 static inline int
-check_and_atoi (const char *str IN)
+atoi_and_check (const char *num IN)
 {
-  int i, tmp, res;
+  int i, tmp, res = 0;
 
-  if (unlikely(str == NULL))
+  if (unlikely(num == NULL))
     {
-      printf("[check_out_atoi] String empty Error!\n");
+      printf("[atoi_after_check] String empty Error!\n");
       return -1;
     }
 
-  for (i = 0; str[i]; ++i)
+  for (i = 0; num[i]; ++i)
     {
-      tmp = str[i] - 0x30;
+      tmp = num[i] - 0x30;
       if (unlikely(tmp < 0 || tmp > 9))
         {
           perror("[check_out_atoi] Arguments Error\n");
@@ -93,16 +106,17 @@ check_and_atoi (const char *str IN)
   return res;
 }
 
+/*
 static inline struct converted_argv
 check_argv(char **argv IN)
 {
-  /* Declaration */
+   Declaration
   struct converted_argv conv_argv;
 
-  conv_argv.time_interval  = check_and_atoi(argv[1]);
-  conv_argv.number         = check_and_atoi(argv[2]);
-  conv_argv.startup_option = check_and_atoi(argv[3]);
+  conv_argv.time_interval  = atoi_and_check(argv[1]);
+  conv_argv.number         = atoi_and_check(argv[2]);
+  conv_argv.startup_option = atoi_and_check(argv[3]);
 
   return conv_argv;
 }
-
+*/
